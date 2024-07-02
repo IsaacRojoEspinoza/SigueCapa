05e33d6994d9ccfb3db015d2d35db8c1651d78a8
ROJO ESPINOZA MARIO ISAAC <mario.rojo@ine.mx>
2024-07-02 11:22:27 -0600

    se agregan elementos al dashboard

--- app/MoonShine/Pages/Dashboard.php
+++ app/MoonShine/Pages/Dashboard.php
@@ -5,7 +5,17 @@ declare(strict_types=1);
 namespace App\MoonShine\Pages;
 
 use MoonShine\Pages\Page;
+use App\Models\User;
 use MoonShine\Components\MoonShineComponent;
+use MoonShine\Decorations\Column;
+use MoonShine\Decorations\Grid;
+use MoonShine\Decorations\LineBreak;
+use MoonShine\Decorations\TextBlock;
+use MoonShine\Metrics\DonutChartMetric;
+use MoonShine\Metrics\LineChartMetric;
+use MoonShine\Metrics\ValueMetric;
+use App\MoonShine\Resources\UserResource;
+
 
 class Dashboard extends Page
 {
@@ -22,6 +32,7 @@ class Dashboard extends Page
     public function title(): string
     {
         return $this->title ?: 'Pantalla Principal';
+
     }
 
     /**
@@ -29,6 +40,42 @@ class Dashboard extends Page
      */
     public function components(): array
 	{
-		return [];
+		return [
+            LineBreak::make(),
+
+            Grid::make([
+                Column::make([
+                    ValueMetric::make('Almunos Inscritos')
+                        ->value(User::query()->count()),
+                ])->columnSpan(6),
+
+                Column::make([
+                    ValueMetric::make('Alumnos Con Acceso a la plataforma')
+                        ->value(User::query()->count()),
+                ])->columnSpan(6),
+
+                Column::make([
+                    DonutChartMetric::make('Accesos')
+                        ->columnSpan(6)
+                        ->values(['Con acceso' => 10000, 'Sin acceso' => 3000]),
+                ])->columnSpan(6),
+
+                Column::make([
+                    LineChartMetric::make('Avance Meta')
+                        ->line([
+                            'Avance por Entidad' => [
+                                now()->format('Y-m-d') => 100,
+                                now()->addDay()->format('Y-m-d') => 200
+                            ]
+                        ])
+                        ->line([
+                            'Meta por Entidad' => [
+                                now()->format('Y-m-d') => 300,
+                                now()->addDay()->format('Y-m-d') => 400
+                            ]
+                        ], '#EC4176'),
+                ])->columnSpan(6)
+            ]),
+        ];
 	}
 }
